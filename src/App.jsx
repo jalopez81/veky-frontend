948e42ffaa1edb380a344b06376d76924cab6f38
George L <jorge.lopez@newfold.com>
2026-02-15 20:49:37 -0400

    set default language

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,7 @@ import UsersReportPage from './pages/reports/users';
 import WishlistPage from './pages/WishlistPage';
 import Navbar from './components/navbar';
 import Footer from './components/Footer';
+import i18next from 'i18next';
 
 ModuleRegistry.registerModules([AllCommunityModule]);
 
@@ -65,6 +66,8 @@ const theme = createTheme({
 console.clear();
 console.log('App.jsx: console cleared');
 
+i18next.init({ lng: 'en', debug: false, });
+
 const App = () => {
   return (
     <AppProvider>
@@ -74,133 +77,133 @@ const App = () => {
             <Navbar />
             <ScrollToTop />
             <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/test" element={<TestPage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/about-us" element={<AboutUsPage />} />
-            <Route
-              path="/contactus"
-              element={
-                <PageContainer>
-                  <PageHeader>
-                    <Box
-                      sx={{
-                        display: 'flex',
-                        gap: 2,
-                        justifyContent: 'flex-end',
-                      }}
-                    >
-                      <NavigationButton href="/products" text="Productos ►" />
-                    </Box>
-                  </PageHeader>
-                  <ContactUsPage />
-                </PageContainer>
-              }
-            />
-            <Route path="/products" element={<ProductsPage />} />
-            <Route
-              path="/products/:productId"
-              element={<ProductDetailPage />}
-            />
-            <Route
-              path="/orders"
-              element={
-                <ProtectedRoute allowedRoles={[ROLES.admin, ROLES.customer]}>
-                  <OrdersPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="/cart" element={<CartPage />} />
-            <Route path="/wishlist" element={<WishlistPage />} />
-            <Route path="/profile" element={<UserProfilePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/my-orders" element={<MyOrdersPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/questionnare" element={<QuestionnairePage />} />
-            <Route
-              path="/users/roles"
-              element={
-                <ProtectedRoute allowedRoles={[ROLES.admin]}>
-                  <UserRolesPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/products/inventory"
-              element={
-                <ProtectedRoute allowedRoles={[ROLES.admin]}>
-                  <InventoryPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/products/add-new-product"
-              element={
-                <ProtectedRoute allowedRoles={[ROLES.admin]}>
-                  <AddNewProductPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/reports/sales"
-              element={
-                <ProtectedRoute allowedRoles={[ROLES.admin]}>
-                  <SalesReportPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/reports/pending-orders"
-              element={
-                <ProtectedRoute allowedRoles={[ROLES.admin]}>
-                  <PendingOrdersReportPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/reports/inventory"
-              element={
-                <ProtectedRoute allowedRoles={[ROLES.admin]}>
-                  <InventoryReportPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/reports/top-selling"
-              element={
-                <ProtectedRoute allowedRoles={[ROLES.admin]}>
-                  <TopSellingProductsReportPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/reports/users"
-              element={
-                <ProtectedRoute allowedRoles={[ROLES.admin]}>
-                  <UsersReportPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/reports/reviews"
-              element={
-                <ProtectedRoute allowedRoles={[ROLES.admin]}>
-                  <ReviewsReportPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/reports/contactus"
-              element={
-                <ProtectedRoute allowedRoles={[ROLES.admin]}>
-                  <ContactUsReportPage />
-                </ProtectedRoute>
-              }
-            />
-          </Routes>
-          <Footer />
-        </Router>
-      </ThemeProvider>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/test" element={<TestPage />} />
+              <Route path="/home" element={<HomePage />} />
+              <Route path="/about-us" element={<AboutUsPage />} />
+              <Route
+                path="/contactus"
+                element={
+                  <PageContainer>
+                    <PageHeader>
+                      <Box
+                        sx={{
+                          display: 'flex',
+                          gap: 2,
+                          justifyContent: 'flex-end',
+                        }}
+                      >
+                        <NavigationButton href="/products" text="Productos ►" />
+                      </Box>
+                    </PageHeader>
+                    <ContactUsPage />
+                  </PageContainer>
+                }
+              />
+              <Route path="/products" element={<ProductsPage />} />
+              <Route
+                path="/products/:productId"
+                element={<ProductDetailPage />}
+              />
+              <Route
+                path="/orders"
+                element={
+                  <ProtectedRoute allowedRoles={[ROLES.admin, ROLES.customer]}>
+                    <OrdersPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="/cart" element={<CartPage />} />
+              <Route path="/wishlist" element={<WishlistPage />} />
+              <Route path="/profile" element={<UserProfilePage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/my-orders" element={<MyOrdersPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route path="/questionnare" element={<QuestionnairePage />} />
+              <Route
+                path="/users/roles"
+                element={
+                  <ProtectedRoute allowedRoles={[ROLES.admin]}>
+                    <UserRolesPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/products/inventory"
+                element={
+                  <ProtectedRoute allowedRoles={[ROLES.admin]}>
+                    <InventoryPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/products/add-new-product"
+                element={
+                  <ProtectedRoute allowedRoles={[ROLES.admin]}>
+                    <AddNewProductPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/reports/sales"
+                element={
+                  <ProtectedRoute allowedRoles={[ROLES.admin]}>
+                    <SalesReportPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/reports/pending-orders"
+                element={
+                  <ProtectedRoute allowedRoles={[ROLES.admin]}>
+                    <PendingOrdersReportPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/reports/inventory"
+                element={
+                  <ProtectedRoute allowedRoles={[ROLES.admin]}>
+                    <InventoryReportPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/reports/top-selling"
+                element={
+                  <ProtectedRoute allowedRoles={[ROLES.admin]}>
+                    <TopSellingProductsReportPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/reports/users"
+                element={
+                  <ProtectedRoute allowedRoles={[ROLES.admin]}>
+                    <UsersReportPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/reports/reviews"
+                element={
+                  <ProtectedRoute allowedRoles={[ROLES.admin]}>
+                    <ReviewsReportPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/reports/contactus"
+                element={
+                  <ProtectedRoute allowedRoles={[ROLES.admin]}>
+                    <ContactUsReportPage />
+                  </ProtectedRoute>
+                }
+              />
+            </Routes>
+            <Footer />
+          </Router>
+        </ThemeProvider>
       </I18nProvider>
     </AppProvider>
   );
